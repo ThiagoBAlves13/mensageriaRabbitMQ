369cf432bab589a050f9651d6830321ef2abaa26
Thiago Barreto Alves <thiagobalves13@gmail.com>
2025-06-17 17:04:12 -0300

    enviando e recebendo mensagem RabbitMQ

--- pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -1,18 +1,25 @@
 package br.com.alurafood.pedidos.controller;
 
-import br.com.alurafood.pedidos.dto.PedidoDto;
-import br.com.alurafood.pedidos.dto.StatusDto;
-import br.com.alurafood.pedidos.service.PedidoService;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.net.URI;
-import java.util.List;
+import br.com.alurafood.pedidos.dto.PedidoDto;
+import br.com.alurafood.pedidos.dto.StatusDto;
+import br.com.alurafood.pedidos.service.PedidoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/pedidos")
--- pedidos/src/main/java/br/com/alurafood/pedidos/model/ItemDoPedido.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/model/ItemDoPedido.java
@@ -1,18 +1,20 @@
 package br.com.alurafood.pedidos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Entity
 @Table(name = "item_do_pedido")
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemDoPedido {
