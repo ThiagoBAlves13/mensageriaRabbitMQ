f21eef227c0303b508491a110137f32979a03014
Thiago Barreto Alves <thiagobalves13@gmail.com>
2025-06-19 15:32:50 -0300

    DLQ e DLX

--- avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
+++ avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
@@ -1,14 +1,16 @@
 package br.com.alurafood.avaliacao.avaliacao.amqp;
 
-import br.com.alurafood.avaliacao.avaliacao.dto.PagamentoDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import br.com.alurafood.avaliacao.avaliacao.dto.PagamentoDto;
 
 @Component
 public class AvaliacaoListener {
     @RabbitListener(queues = "pagamentos.detalhes-avaliacao")
     public void recebeMensagem(@Payload PagamentoDto pagamento) {
+
         String mensagem = """
                 Necessário criar registro de avaliação para o pedido: %s 
                 Id do pagamento: %s
--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,39 +19,56 @@ import org.springframework.context.annotation.Configuration;
 public class PedidoAMQPConfiguration {
 
     @Bean
-    public Jackson2JsonMessageConverter messageConverter(){
+    public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter){
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
 
     @Bean
-    public Queue filhaDetalhesPedido(){
-        return new Queue("pagamentos.detalhes-pedido", false);
+    public Queue filaDetalhesPedido() {
+        return QueueBuilder.nonDurable("pagamentos.detalhes-pedido")
+                .deadLetterExchange("pagamentos.dlx").build();
     }
 
     @Bean
-    public FanoutExchange fanoutExchange(){
+    public FanoutExchange fanoutExchange() {
         return ExchangeBuilder.fanoutExchange("pagamentos.ex").build();
     }
 
     @Bean
-    public Binding bindPagamentoPedido(FanoutExchange fanoutExchange){
-        return BindingBuilder.bind(filhaDetalhesPedido()).to(fanoutExchange);
+    public Binding bindPagamentoPedido(FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(filaDetalhesPedido()).to(fanoutExchange);
     }
 
-        @Bean
-    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn){
+    @Bean
+    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
         return new RabbitAdmin(conn);
     }
 
     @Bean
-    public ApplicationListener<ApplicationReadyEvent> inicializaRabbitAdmin(RabbitAdmin rabbitAdmin){
+    public ApplicationListener<ApplicationReadyEvent> inicializaRabbitAdmin(RabbitAdmin rabbitAdmin) {
         return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange deadLetterExchange() {
+        return ExchangeBuilder.fanoutExchange("pagamentos.dlx").build();
+    }
+
+    @Bean
+    public Queue filaDlqDetalhesPedido() {
+        return new Queue("pagamentos.detalhes-pedido-dlq", false);
+    }
+
+    @Bean
+    public Binding bindDlxPagamentoPedido() {
+        return BindingBuilder.bind(filaDlqDetalhesPedido()).to(deadLetterExchange());
     }
 }
